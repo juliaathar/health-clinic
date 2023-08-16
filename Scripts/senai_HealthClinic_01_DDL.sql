97b5fa15bb75dadff39e5badb48885786b4308cb
juliaathar <juathar@gmail.com>
2023-08-16 09:21:13 -0300

    trabalhando dml do projeto

--- Scripts/senai_HealthClinic_01_DDL.sql
+++ Scripts/senai_HealthClinic_01_DDL.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE Health_Clinic_Manhã
+CREATE DATABASE Health_Clinic_Júlia
 
 CREATE TABLE TipoDeUsuario
 (
@@ -10,6 +10,7 @@ CREATE TABLE Usuario
 (
   IdUsuario INT PRIMARY KEY IDENTITY,
   IdTipoDeUsuario INT FOREIGN KEY REFERENCES TipoDeUsuario(IdTipoDeUsuario),
+  NomeUsuario VARCHAR(100) NOT NULL,
   Email VARCHAR(256) NOT NULL,
   Senha VARCHAR(100) NOT NULL 
 )
@@ -18,8 +19,7 @@ CREATE TABLE Paciente
 (
   IdPaciente INT PRIMARY KEY IDENTITY,
   IdUsuario INT FOREIGN KEY REFERENCES Usuario(IdUsuario) NOT NULL UNIQUE,
-  NomePaciente VARCHAR(100) NOT NULL,
-  RG VARCHAR(7) NOT NULL UNIQUE,
+  RG VARCHAR(9) NOT NULL UNIQUE,
   CPF VARCHAR(11) NOT NULL UNIQUE,
   DataNascimento DATE NOT NULL, 
   Telefone VARCHAR(15) 
@@ -29,12 +29,6 @@ CREATE TABLE Especialidade
 (
   IdEspecialidade INT PRIMARY KEY IDENTITY,
   NomeEspecialidade VARCHAR(100) NOT NULL
-)
-
-CREATE TABLE MedEspecialidade
-(
-  IdMedEspecialidade INT PRIMARY KEY IDENTITY,
-  IdEspecialidade INT FOREIGN KEY REFERENCES Especialidade(IdEspecialidade)
 )
 
 CREATE TABLE Clinica 
@@ -51,17 +45,23 @@ CREATE TABLE Clinica
 CREATE TABLE Medico
 (
   IdMedico INT PRIMARY KEY IDENTITY,
-  IdMedEspecialidade INT FOREIGN KEY REFERENCES MedEspecialidade(IdMedespecialidade),
   IdClinica INT FOREIGN KEY REFERENCES Clinica (IdClinica),
   IdUsuario INT FOREIGN KEY REFERENCES Usuario (IdUsuario),
-  NomeMedico VARCHAR(100) NOT NULL,
   CRM VARCHAR(6) NOT NULL UNIQUE
+)
+
+CREATE TABLE MedEspecialidade
+(
+  IdMedEspecialidade INT PRIMARY KEY IDENTITY,
+  IdMedico INT FOREIGN KEY REFERENCES Medico(IdMedico),
+  IdEspecialidade INT FOREIGN KEY REFERENCES Especialidade(IdEspecialidade)
 )
 
 CREATE TABLE Comentario
 (
   IdComentario INT PRIMARY KEY IDENTITY,
-  IdPaciente INT FOREIGN KEY REFERENCES Paciente (IdPaciente)
+  IdPaciente INT FOREIGN KEY REFERENCES Paciente (IdPaciente),
+  Avaliacao VARCHAR(256) NOT NULL
 )
 
 CREATE TABLE Consulta 
--- Scripts/senai_HealthClinic_02_DML.sql
+++ Scripts/senai_HealthClinic_02_DML.sql
@@ -1,2 +1,38 @@
 INSERT INTO TipoDeUsuario (Tipo)
-VALUES ('Administrador'), ('Médico'), ('Paciente')
+VALUES ('Administrador'), ('Médico'), ('Paciente');
+
+INSERT INTO Usuario (IdTipoDeUsuario, NomeUsuario, Email, Senha)
+VALUES (1, 'Lucas Pereira', 'lucaspereira@gmail.com', 'lucas123'),
+       (2, 'Júlia Aben-Athar', 'julia8@gmail.com', 'julia777'),
+	   (2, 'Raphaela Ranyol', 'raphaela.ra@gmail.com', 'nWQa2o1'),
+	   (2, 'Guilherme Cordeiro', 'guicord@gmail.com', 'cordeiro12034'),
+	   (3,'Carlos Roque', 'carlosrq2@gmail.com', 'roque23'),
+	   (3,'Eduardo Costa', 'ducosta@gmail.com', 'Ns3JBs9'),
+	   (3,'Henrique Gomes', 'gomesHQ12@gmail', 'henrique123gm');
+
+INSERT INTO Paciente (IdUsuario, RG, CPF, DataNascimento, Telefone)	
+VALUES (5, '12.345.678-9', '123.456.789-09', '08-01-1985', '(11) 98765-6714'),
+       (6, '32.973.823-6', '844.836.243-09', '21-07-1980', '(11) 97264-9284'),
+	   (7, '93.234.093-2', '737.827.298-46', '08-08-1988', '(11) 98274-9272');
+
+INSERT INTO Especialidade (NomeEspecialidade)
+VALUES ('Cardiologista'),
+       ('Psiquiatra'),
+	   ('Pediatra'),
+	   ('Ortopedista');
+
+INSERT INTO Clinica (NomeFantasia, Endereco, CNPJ, [Site], HorarioAbertura, HorarioFechamento)
+VALUES ('Health Clinic', 'Av Paulista, 1200, Jd Paulista, São Paulo', '89.478.454/0001-09', 'healthclinic.com.br', '06:30:00', '20:30:00');
+
+INSERT INTO Medico (IdClinica, IdUsuario, CRM)
+VALUES (1, 2, 'CRM/SP123456'),
+       (1, 3, 'CRM/SP093645'),
+	   (1, 4, 'CRM/SP353489');
+
+INSERT INTO MedEspecialidade (IdMedico, IdEspecialidade)
+VALUES (1, 2),
+       (2, 1),
+	   (3, 3),
+	   (3, 4);
+
+INSERT INTO Comentario (IdPaciente)
